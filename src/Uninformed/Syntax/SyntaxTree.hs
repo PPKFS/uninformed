9004231672753bd6a49f0c52cb5d885c70491555
avery <avery@chordify.net>
2022-10-17 19:32:39 +0200

    more zipper attempts

--- src/Uninformed/Syntax/SyntaxTree.hs
+++ src/Uninformed/Syntax/SyntaxTree.hs
@@ -10,15 +10,27 @@ data Node a = Node
   , _nodeId :: Text
   } deriving stock Show
 
-data SyntaxTree a = Leaf (Node a) | Branch (Node a) [SyntaxTree a] deriving stock (Show)
+data SyntaxTree a = Leaf (Node a) | Branch (Node a) (V.Vector (SyntaxTree a)) deriving stock (Show)
 
-data Crumb a = Crumb (Node a) [SyntaxTree a] [SyntaxTree a] deriving stock (Show)
+data Crumb a = Crumb (Node a) (V.Vector (SyntaxTree a)) (V.Vector (SyntaxTree a)) deriving stock (Show)
 
 type Zipper a = (SyntaxTree a, [Crumb a])
 
 up :: Zipper a -> Zipper a
 up (n, []) = (n, [])
-up (n, (Crumb node ls rs):bs) = (Branch node (V.concat [ls, one n, rs]), bs)
+up (n, (Crumb node ls rs):bs) = (Branch node (V.concat [ls, V.singleton n, rs]), bs)
+
+left :: Zipper a -> Zipper a
+left (n, c) = fromMaybe (n, []) $ do
+  ((Crumb node ls rs), bs) <- uncons c
+  (ls', l) <- V.unsnoc ls
+  return (l, (Crumb node ls' $ V.cons n rs):bs)
+
+right :: Zipper a -> Zipper a
+right (n, c) = fromMaybe (n, []) $ do
+  ((Crumb node ls rs), bs) <- uncons c
+  (r, rs') <- V.uncons rs
+  return (r, (Crumb node (V.snoc ls n) rs'):bs)
 
 graftSentence ::
   Node a
