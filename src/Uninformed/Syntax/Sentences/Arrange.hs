83ebf9652cd0f812c1abf00449bf9a358d93c7c8
avery <avery@chordify.net>
2022-10-18 18:08:19 +0200

    add grafting nodes

--- src/Uninformed/Syntax/Sentences/Arrange.hs
+++ src/Uninformed/Syntax/Sentences/Arrange.hs
@@ -1,15 +1,16 @@
 module Uninformed.Syntax.Sentences.Arrange where
 
 import Uninformed.Syntax.SyntaxTree
-import Uninformed.Words.Lexer
+import Uninformed.Words.Lexer.Types
 import Uninformed.Syntax.Sentences
 
+data HeadingType = Implicit | Explicit
 
 createSyntaxTreeSkeleton ::
   Text
   -> [Sentence]
   -> SyntaxTree Word
-createSyntaxTreeSkeleton fn = fst . foldl' acceptSentence (newSyntaxTree fn)
+createSyntaxTreeSkeleton fn = fst . unzipper . foldl' acceptSentence (newSyntaxTree fn)
 
 acceptSentence ::
   Zipper Word
@@ -18,23 +19,33 @@ acceptSentence ::
 acceptSentence z sentence = do
   detectChangeOfSourceFile z sentence
 
+  -- if this sentence is in a different file to our current focus, then
+  -- we want a new implicit super heading.
 detectChangeOfSourceFile ::
   Zipper Word
   -> Sentence
   -> Zipper Word
 detectChangeOfSourceFile z sentence = if lastFile /= sentenceFileOfOrigin sentence
   then
-    makeNewHeadingNode Implicit 0 (sentenceFileOfOrigin sentence)
+    makeNewHeadingNode Implicit 0 (sentenceFileOfOrigin sentence) z
   else
     z
   where
-    lastFile = focus z ^. nodeSourceLocation
+    lastFile = focus z ^. nodeLocation % sourceLocationFile
+
+makeNewHeadingNode :: 
+  HeadingType 
+  -> Int 
+  -> Maybe Text 
+  -> Zipper Word
+  -> Zipper Word
+makeNewHeadingNode ht hl hfn z = 
+  upWhile (\z' -> getHeadingLevel z' > hl) z & 
+  graftNodeChild (newNode)
+
+newNode :: Node Word
+newNode = _
+  -- if the new heading level is lower than the current heading level, then
+  -- we need to go *up* until we hit a higher level.
 
 
-  --detectDividingSentence parsedSentence
-  -- some skipping checks and also some problems
-  --checkForSkipping
-  --considerHeadingNode
-
-  -- if this sentence is in a different file to our current focus, then
-  -- we want a new implicit super heading.
--- src/Uninformed/Syntax/SyntaxTree.hs
+++ src/Uninformed/Syntax/SyntaxTree.hs
@@ -6,7 +6,7 @@ import qualified Data.Vector as V
 import qualified Data.Set as S
 import Uninformed.Words.Lexer.Types
 
-data NodeType = HeadingNode | RootNode deriving stock (Show)
+data NodeType = HeadingNode Int | RootNode deriving stock (Show)
 
 data Node a = Node
   { _nodeAnnotations :: Set a
@@ -16,21 +16,42 @@ data Node a = Node
   , _nodeId :: Text
   } deriving stock Show
 
-data SyntaxTree a = Leaf (Node a) | Branch (Node a) (V.Vector (SyntaxTree a)) deriving stock (Show)
+instance Eq (Node a) where
+  (==) n1 n2 = _nodeId n1 == _nodeId n2
+
+data SyntaxTree a = Leaf (Node a) | Branch (Node a) (V.Vector (SyntaxTree a)) deriving stock (Show, Eq)
 
 data Crumb a = Crumb (Node a) (V.Vector (SyntaxTree a)) (V.Vector (SyntaxTree a)) deriving stock (Show)
 
-type Zipper a = (SyntaxTree a, [Crumb a])
+newtype Zipper a = Zipper { unzipper :: (SyntaxTree a, [Crumb a]) }
+
+instance Eq (Zipper a) where
+  (==) (Zipper (t1, _)) (Zipper (t2, _)) = t1 == t2
 
 focus ::
   Zipper a
-  -> SyntaxTree a
-focus = fst
+  -> Node a
+focus = getNode . fst . unzipper
+
+getNode ::
+  SyntaxTree a
+  -> Node a
+getNode (Leaf l) = l
+getNode (Branch n _) = n
+
+getHeadingLevel ::
+  Zipper a
+  -> Int
+getHeadingLevel z = upWhile (\n -> case _nodeType . focus $ n of
+  HeadingNode _ -> False
+  _ -> True) z & focus & _nodeType & (\case
+    HeadingNode n -> n
+    _ -> error "impossible")
 
 newSyntaxTree ::
   Text
   -> Zipper a
-newSyntaxTree fn = (Leaf (Node
+newSyntaxTree fn = Zipper (Leaf (Node
   { _nodeAnnotations = S.empty
   , _nodeType = RootNode
   , _nodeRaw = fn
@@ -39,31 +60,50 @@ newSyntaxTree fn = (Leaf (Node
   }), [])
 
 up :: Zipper a -> Zipper a
-up (n, []) = (n, [])
-up (n, (Crumb node ls rs):bs) = (Branch node (V.concat [ls, V.singleton n, rs]), bs)
+up (Zipper (n, [])) = Zipper (n, [])
+up (Zipper (n, (Crumb node ls rs):bs)) = Zipper (Branch node (V.concat [ls, V.singleton n, rs]), bs)
+
+upWhile :: 
+  (Zipper a -> Bool)
+  -> Zipper a
+  -> Zipper a
+upWhile f z = 
+  if f z
+    then 
+      let upN = up z in 
+        if upN == z then error "oops you hit the root"
+        else upWhile f upN
+    else
+      z
 
 left :: Zipper a -> Zipper a
-left (n, c) = fromMaybe (n, []) $ do
+left (Zipper (n, c)) = fromMaybe (Zipper (n, [])) $ do
   (Crumb node ls rs, bs) <- uncons c
   (ls', l) <- V.unsnoc ls
-  return (l, Crumb node ls' (V.cons n rs):bs)
+  return $ Zipper (l, Crumb node ls' (V.cons n rs):bs)
 
 right :: Zipper a -> Zipper a
-right (n, c) = fromMaybe (n, []) $ do
+right (Zipper (n, c)) = fromMaybe (Zipper (n, [])) $ do
   (Crumb node ls rs, bs) <- uncons c
   (r, rs') <- V.uncons rs
-  return (r, Crumb node (V.snoc ls n) rs':bs)
+  return $ Zipper (r, Crumb node (V.snoc ls n) rs':bs)
 
-graftSentence ::
+graftNodeChild ::
   Node a
   -> Zipper a
   -> Zipper a
-graftSentence node = graftTree (Leaf node)
+graftNodeChild node = graftTreeChild (Leaf node)
 
-graftTree ::
+graftTreeChild ::
   SyntaxTree a
   -> Zipper a
   -> Zipper a
-graftTree = error ""
+graftTreeChild t z = error ""
+
+graftTreeSibling ::
+  SyntaxTree a
+  -> Zipper a
+  -> Zipper a
+graftTreeSibling t z = error ""
 
 makeLenses ''Node
